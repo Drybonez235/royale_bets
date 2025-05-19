0536cf7275f1c450175ed2bfd55a35495f6467c5
Jonathan Lewis <jonathan.lewis@welch.edu>
2025-05-19 18:14:02 -0500

    changed absolute paths to relative

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        viewer_sign_up: resolve(__dirname, '/Users/jonathanlewis/Documents/Projects/royale_bets/viewer/player_tag_input.html'),
-        bet_interface: resolve(__dirname, '/Users/jonathanlewis/Documents/Projects/royale_bets/viewer/bet_interface.html'),
-        streamer_sign_up: resolve(__dirname, '/Users/jonathanlewis/Documents/Projects/royale_bets/streamer/streamer_sign_up.html'),
-        streamer_redirect: resolve(__dirname, '/Users/jonathanlewis/Documents/Projects/royale_bets/streamer/streamer_sign_up_redirect.html')
+        viewer_sign_up: resolve(__dirname, '/royale_bets/viewer/player_tag_input.html'),
+        bet_interface: resolve(__dirname, '/royale_bets/viewer/bet_interface.html'),
+        streamer_sign_up: resolve(__dirname, '/royale_bets/streamer/streamer_sign_up.html'),
+        streamer_redirect: resolve(__dirname, '/royale_bets/streamer/streamer_sign_up_redirect.html')
       },
     },
   },
