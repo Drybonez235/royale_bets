51bc4814ecf03dab27d574fb052b30ff40aef365
Jonathan Lewis <jonathan.lewis@welch.edu>
2025-05-19 18:18:29 -0500

    This time

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        viewer_sign_up: resolve(__dirname, '/royale_bets/viewer/player_tag_input.html'),
-        bet_interface: resolve(__dirname, '/royale_bets/viewer/bet_interface.html'),
-        streamer_sign_up: resolve(__dirname, '/royale_bets/streamer/streamer_sign_up.html'),
-        streamer_redirect: resolve(__dirname, '/royale_bets/streamer/streamer_sign_up_redirect.html')
+        viewer_sign_up: resolve(__dirname, '/viewer/player_tag_input.html'),
+        bet_interface: resolve(__dirname, '/viewer/bet_interface.html'),
+        streamer_sign_up: resolve(__dirname, '/streamer/streamer_sign_up.html'),
+        streamer_redirect: resolve(__dirname, '/streamer/streamer_sign_up_redirect.html')
       },
     },
   },
